9c738087037aeabe86af3d1135ccac657994012f
TachibanaNero <victor.costa.brasil@gmail.com>
2021-06-23 01:26:23 -0300

    Configuração do Projeto

---
+++ src/services/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9xzGWOZsQvt1-pdYyZEmFxTVdGNobhI8",
+  authDomain: "reactproject-letmeask.firebaseapp.com",
+  databaseURL: "https://reactproject-letmeask-default-rtdb.firebaseio.com",
+  projectId: "reactproject-letmeask",
+  storageBucket: "reactproject-letmeask.appspot.com",
+  messagingSenderId: "231631207463",
+  appId: "1:231631207463:web:dca26c39ff3d70ed61a202"
+};
+
+firebase.initializeApp(firebaseConfig)
